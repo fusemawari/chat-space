627bb451298582fde569c9e24888c5557fbbdf16
fusemawari <fusemawari@gmail.com>
2018-04-11 15:09:14 +0900

    4月10日までの作業

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,45 @@
+$(function(){
+  function buildHTML(message){
+    var image_url = (message.image_url)? `<image class="lower-message_image" src="${message.image_url}">`:"";
+    var html = `<ul class="message">
+                  <li class="messages__list">
+                    <span class="messages__date">${ message.name }
+                    </span>
+                    <div class="upper-message__date">
+                    <p>${ message.created_at }</p>
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                    <p>${ message.content }</p>
+                    ${ message.image == null ? "" : '<img src="' + message.image + '">' }
+                    </p>
+                  </div>
+              </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
     $.ajax({
-      url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-  })
-})
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html);
+      $('.form_message').val('');
+      $('.hidden').val('')
+      $('.form__submit').attr('disabled',false);
+      $('.message').animate({scrollTop: $('.message')[0].scrollHeight }, 1000);
+      })
+    .fail(function(data){
+      alert('入力してください');
+      $(".form__submit").attr("disabled",false);
+    })
+  });
+});
+
